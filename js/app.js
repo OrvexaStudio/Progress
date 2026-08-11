e71c1362216fe9c5cf632fd822c89b8d99fceafb
OrvexaStudio <phw966j56w@privaterelay.appleid.com>
2026-08-12 01:06:11 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,53 @@
 applyProgressTheme();
 
+
 document.addEventListener(
     "DOMContentLoaded",
     () => {
 
+
         loadData();
 
+
+        const name =
+            getUserName();
+
+
+        const tutorialDone =
+            localStorage.getItem(
+                "progress_tutorial_completed"
+            );
+
+
+
+        if(!name){
+
+            showWelcomeScreen();
+
+            return;
+
+        }
+
+
+
+        if(!tutorialDone){
+
+            startTutorial();
+
+            return;
+
+        }
+
+
+
         initializeNavigation();
+
         initializeMobileSidebar();
 
         updateGlobalProfile();
 
         navigateTo("dashboard");
+
 
     }
 );

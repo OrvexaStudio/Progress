5966331aecaf1a1710c31ff1106fe8b57a25fc90
OrvexaStudio <phw966j56w@privaterelay.appleid.com>
2026-08-12 15:42:09 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,3 +51,31 @@ document.addEventListener(
 
     }
 );
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker.register(
+            "./service-worker.js"
+        )
+        .then(registration => {
+
+            console.log(
+                "Service Worker registrato:",
+                registration.scope
+            );
+
+        })
+        .catch(error => {
+
+            console.error(
+                "Errore Service Worker:",
+                error
+            );
+
+        });
+
+    });
+
+}
